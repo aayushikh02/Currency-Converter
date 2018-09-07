681c1b1be97df0686182eb0abbcaf2245cb04c24
aayushikh02 <aayushi.kh02@gmailcom>
2018-09-07 20:05:27 +0530

    Currency converter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-}
+  base="INR";
+  case="USD";
+  amount=1;
+  answer=0.014606643;
+  ans
+  url="https://api.exchangeratesapi.io/latest?base=";
+
+  constructor(public http:HttpClient){
+  }
+
+    conversion(){
+      this.http.get(this.url+this.base).subscribe((data)=>{
+        console.log(data);
+        console.log(this.base);
+        console.log(this.case);
+        this.ans=data;
+        this.answer=this.amount*this.ans.rates[this.case];
+      })
+
+    }
+  }
+
